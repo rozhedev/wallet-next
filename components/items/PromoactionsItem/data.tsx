84c402f3123b6ddfb123f1f7d6ab22562b873f72
rozhedev <digital1479@gmail.com>
2023-12-14 20:00:28 +0200

    Fill promoactionsItemArr. Minor stylisation changes

--- components/items/PromoactionsItem/data.tsx
+++ components/items/PromoactionsItem/data.tsx
@@ -13,7 +13,9 @@ export const promoactionsItemArr: TPromoactionsItemArr = [
     },
     {
         id: 2,
-        title: "Avalanche (AVAX) aviable now. Native ecosystem support. Get 0.01 AVAX airdrop.",
+        title: (
+            <>Avalanche (AVAX) available now. <br/> Native ecosystem support. Get 0.01 AVAX airdrop.</>
+        ),
         tags: ["#flemwallet", "#avalanche", "#avax"],
         btnLabel: "Get airdrop",
         isLink: false,
@@ -21,7 +23,9 @@ export const promoactionsItemArr: TPromoactionsItemArr = [
     },
     {
         id: 3,
-        title: "Polygon (MATIC) aviable now. Native ecosystem support. Get 5 MATIC airdrop.",
+        title: (
+            <>Polygon (MATIC) available now. <br/> Native ecosystem support. Get 5 MATIC airdrop.</>
+        ),
         tags: ["#flemwallet", "#polygon", "#matic"],
         btnLabel: "Get airdrop",
         isLink: false,
@@ -29,7 +33,9 @@ export const promoactionsItemArr: TPromoactionsItemArr = [
     },
     {
         id: 4,
-        title: "New version our app. Optimisation and new features. Added UTXO’s control.",
+        title: (
+            <>New version our app. <br/> Optimisation and new features. Added UTXO’s control.</>
+        ),
         tags: ["#flemwallet", "#update", "#utxo"],
         btnLabel: "See changelog",
         isLink: true,
@@ -37,8 +43,110 @@ export const promoactionsItemArr: TPromoactionsItemArr = [
     },
     {
         id: 5,
-        title: "Stellar (XLM) aviable now. Native ecosystem support. Get 100 XLM airdrop.",
+        title: (
+            <>Stellar (XLM) available now. <br/> Native ecosystem support. Get 100 XLM airdrop.</>
+        ),
         tags: ["#flemwallet", "#stellar", "#xlm"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 6,
+        title: (
+            <>Chainlink (LINK) available now. <br/> Native ecosystem support. Get 50 LINK airdrop.</>
+        ),
+        tags: ["#flemwallet", "#chainlink", "#link"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 7,
+        title: (
+            <>Update cryptocurrency API for the faster access to actual course.</>
+        ),
+        tags: ["#flemwallet", "#update", "#API"],
+        btnLabel: "See changelog",
+        isLink: true,
+        linkHref: "https://github.com/airgap-it/airgap-wallet",
+    },
+    {
+        id: 8,
+        title: (
+            <>Polkadot (DOT) available now. <br/> Native ecosystem support. Get 10 DOT airdrop.</>
+        ),
+        tags: ["#flemwallet", "#polkadot", "#dot"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 9,
+        title: (
+            <>Algorand (ALGO) available now. <br/> Native ecosystem support. Get 200 ALGO airdrop.</>
+        ),
+        tags: ["#flemwallet", "#algorand", "#algo"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 10,
+        title: (
+            <>1 year with Flem Wallet. We <br/> continue to add new currencies and functionality.</>
+        ),
+        tags: ["#flem", "#flemwallet", "#levelone"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 11,
+        title: (
+            <>Terra (LUNA) available now. <br/> Native ecosystem support. Get 100 LUNA airdrop.</>
+        ),
+        tags: ["#flem", "#terra", "#luna"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 12,
+        title: (
+            <>NEO (NEO) available now. <br/> Native ecosystem support. Get 2 NEO airdrop</>
+        ),
+        tags: ["#flemwallet", "#neo-ecosystem", "#neo"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 13,
+        title: (
+            <>Compound Coin (COMP) available now. Native ecosystem support. Get 0.5 COMP airdrop.</>
+        ),
+        tags: ["#flemwallet", "#compoundcoin", "#comp"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 14,
+        title: (
+            <>Tezos (XTZ) available now. <br/> Native ecosystem support. Get 15 XTZ airdrop.</>
+        ),
+        tags: ["#flemwallet", "#tezos", "#xtz"],
+        btnLabel: "Get airdrop",
+        isLink: false,
+        linkHref: "",
+    },
+    {
+        id: 15,
+        title: (
+            <>Cronos (CRO) available now. <br/> Native ecosystem support. Get 15 CRO airdrop.</>
+        ),
+        tags: ["#flemwallet", "#cronos", "#cro"],
         btnLabel: "Get airdrop",
         isLink: false,
         linkHref: "",
