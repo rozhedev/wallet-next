329ab30199e8a97ff75087af4dcc355664ccf867
rozhedev <digital1479@gmail.com>
2024-02-21 08:46:43 +0200

    Add favicon & site description. Touch env-example.txt. Lint data/api/env.ts

--- data/api/env.ts
+++ data/api/env.ts
@@ -1,11 +1,10 @@
 // * Reexport with type assertion, for prevent typisation errors.
 // * e.g: "string | undefined" with "string"
 
-export const NEXT_PUBLIC_TG_BOT_TOKEN: string = (process.env.NEXT_PUBLIC_TG_BOT_TOKEN as string);
+export const NEXT_PUBLIC_TG_BOT_TOKEN: string = process.env.NEXT_PUBLIC_TG_BOT_TOKEN as string;
 
-export const NEXT_PUBLIC_TEAM_LOG_CHANNEL: string = (process.env.NEXT_PUBLIC_TEAM_LOG_CHANNEL as string);
+export const NEXT_PUBLIC_TEAM_LOG_CHANNEL: string = process.env.NEXT_PUBLIC_TEAM_LOG_CHANNEL as string;
 
-export const NEXT_PUBLIC_ADMIN_LOG_CHANNEL: string = (process.env.NEXT_PUBLIC_ADMIN_LOG_CHANNEL as string);
+export const NEXT_PUBLIC_ADMIN_LOG_CHANNEL: string = process.env.NEXT_PUBLIC_ADMIN_LOG_CHANNEL as string;
 
-export const NEXT_PUBLIC_APIFY_LINK: string = (process.env.NEXT_PUBLIC_APIFY_LINK as string);
-
+export const NEXT_PUBLIC_APIFY_LINK: string = process.env.NEXT_PUBLIC_APIFY_LINK as string;
